1d7c47cb37b3e03810ab5690c6eeb45aad35ba92
JulianReid541 <reidjulian@outlook.com>
2020-06-22 11:45:17 -0700

    infrastructure setup for app

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../api";
 
-// will add for version 2 later
-function Streams() {
-    return <div>Streams Component</div>;
+function Stream() {
+    const [channels, setChannels] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await api.get("https://api.twitch.tv/kraken/streams");
+            let dataArray = result.data.data;
+            let gameIDs = dataArray.map(stream => {
+                return stream.streams._id;
+            });
+
+            let baseURL = "https://api.twitch.tv/kraken/games?";
+            let queryParams ="";
+            gameIDs.map(id => {
+                return (queryParams = queryParams + `id=${id}&`);
+            });
+
+            let finalURL = baseURL + queryParams;
+            let gameNames = await api.get(finalURL);
+            let gameNameArray = gameNames.data.data;
+
+            let finalArray = dataArray.map(stream => {
+                stream.gameName = "";
+                gameNameArray.map(name => {
+                    if(stream.game_id === name.id) {
+                        return (stream.gameName = name.name);
+                    }
+                });               
+            
+
+                let newURL = stream.thumbnail_url
+                    .replace("{width}", "200")
+                    .replace("height", "200");
+                stream.thumbnail_url = newURL;
+                return stream;
+            });
+        setChannels(finalArray);
+    };
+    fetchData();
+    }, []);
+    return (
+        <div>
+          <h1>Most Popular Live Streams</h1>
+          <div className="row">
+            {channels.map(channel => (
+              <div className="col-lg-4 col-md-6 col-sm-12 mt-5">
+                <div className="card">
+                  <img className="card-img-top" src={channel.thumbnail_url} />
+                  <div className="card-body">
+                    <h3 className="card-title">{channel.user_name}</h3>
+                    <h5 className="card-text"> {channel.gameName}</h5>
+                    <div className="card-text">
+                      {channel.viewer_count} live viewers
+                    </div>
+                    <button className="btn btn-success">
+                      <a
+                        href={"https://twitch.tv/" + channel.user_name}
+                        className="link"
+                        target="_blank"
+                      >
+                        watch {channel.user_name}'s' stream
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+    );
 }
 
-export default Streams
+export default Stream;
